b6d67092148f2ffccae48c4b09578d107107e839
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-11-10 20:29:16 -0700

    fix a bug

--- src/wrappers/monitor_wrappers.py
+++ src/wrappers/monitor_wrappers.py
@@ -428,7 +428,7 @@ class RandomNonZeroMonitor(Monitor):
     def _monitor_step(self, action, env_reward):
         env_next_obs = self.env.unwrapped.get_state()
         monitor_reward = 0.0
-        if env_reward != 0 and self.np_random.random() < self.prob and (env_next_obs not in self.forbidden_states):
+        if env_reward != 0 and self.np_random.random() < self.prob or (env_next_obs in self.forbidden_states):
             proxy_reward = np.nan
         else:
             proxy_reward = env_reward
@@ -469,8 +469,8 @@ class RandomMonitor(Monitor):
     def _monitor_step(self, action, env_reward):
         env_next_obs = self.env.unwrapped.get_state()
         monitor_reward = 0.0
-        if self.np_random.random() < self.prob[self.unwrapped.get_state(), action["env"]] and (
-                env_next_obs not in self.forbidden_states):
+        if self.np_random.random() < self.prob[self.unwrapped.get_state(), action["env"]] or (
+                env_next_obs in self.forbidden_states):
             proxy_reward = np.nan
         else:
             proxy_reward = env_reward
