e04025e6b52a7b90a2087ebdcc4f2cde118acca0
alirezakazemipour <kazemipour.alireza@gmail.com>
2024-11-11 13:54:58 -0700

    Add n-expert monitor

--- src/wrappers/monitor_wrappers.py
+++ src/wrappers/monitor_wrappers.py
@@ -264,7 +264,7 @@ class ButtonMonitor(Monitor):
         return obs, reward, terminated, truncated, env_info
 
 
-class NMonitor(Monitor):
+class NSupporterMonitor(Monitor):
     """
     There are N monitors. At every time step, a random monitor is on.
     If the agent's action matches the monitor state, the agent observes the
@@ -321,6 +321,72 @@ class NMonitor(Monitor):
         else:
             proxy_reward = np.nan
             monitor_reward = self.monitor_bonus
+
+        self.monitor_state = self.observation_space["mon"].sample()
+        return self._monitor_get_state(), proxy_reward, monitor_reward, False
+
+
+class NExpertMonitor(Monitor):
+    """
+    There are N monitors. At every time step, a random monitor is on.
+    If the agent's action matches the monitor state, the agent observes the
+    environment reward but receives a negative monitor reward.
+    Otherwise it does not observe the environment reward, but receives a smaller
+    positive monitor reward.
+    For example, if state = 2 and action = 2, the agent observes the environment
+    reward and gets reward_monitor = -0.2.
+    If state = 2 and action != 2, the agent does not observe the reward but
+    gets reward_monitor = 0.001.
+
+    Args:
+        env (gymnasium.Env): the Gymnasium environment,
+        n_monitors (int): number of monitors,
+        monitor_cost (float): cost for observing the reward,
+        monitor_bonus (float): reward for not observing the reward.
+    """
+
+    def __init__(self, env, monitor_cost=0.2, monitor_bonus=0.001, **kwargs):
+        Monitor.__init__(self, env, **kwargs)
+        self.n_monitors = kwargs["n_monitors"]
+        self.action_space = spaces.Dict({
+            "env": env.action_space,
+            "mon": spaces.Discrete(self.n_monitors + 1),
+        })  # fmt: skip
+        self.observation_space = spaces.Dict({
+            "env": env.observation_space,
+            "mon": spaces.Discrete(self.n_monitors),
+        })  # fmt: skip
+        self.monitor_state = 0
+        self.monitor_cost = monitor_cost
+        self.monitor_bonus = monitor_bonus
+        self.prob = kwargs["prob"]
+        self.forbidden_states = kwargs["forbidden_states"] if kwargs["forbidden_states"] is not None else []
+
+    def _monitor_set_state(self, state):
+        self.monitor_state = state
+
+    def _monitor_get_state(self):
+        return np.array(self.monitor_state)
+
+    def _monitor_step(self, action, env_reward):
+        assert (action["mon"] < self.action_space["mon"].n), "illegal monitor action"
+
+        env_next_obs = self.env.unwrapped.get_state()
+
+        p = self.np_random.random()
+        if action["mon"] == self.monitor_state:
+            if p < self.prob and (env_next_obs not in self.forbidden_states):
+                proxy_reward = env_reward
+            else:
+                proxy_reward = np.nan
+            monitor_reward = -self.monitor_cost
+        else:
+            proxy_reward = np.nan
+            monitor_reward = -self.monitor_bonus
+
+        if action["mon"] == self.n_monitors:
+            proxy_reward = np.nan
+            monitor_reward = 0
 
         self.monitor_state = self.observation_space["mon"].sample()
         return self._monitor_get_state(), proxy_reward, monitor_reward, False
